edb4a485a38d6cea0099d4484dcfb236b0c32532
anukollon <anukollon@gmail.com>
2020-11-10 18:52:53 -0600

    Complete

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private Dictionary<string,string> Events = new Dictionary<string, string>();
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name,desc);
+            return Redirect("/Events");
+        }
+    }
+}
