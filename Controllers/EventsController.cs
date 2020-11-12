531f07ecfb00a8ba0723ed65ac754479d0e86eef
anukollon <anukollon@gmail.com>
2020-11-12 12:13:34 -0600

    Added Edit feature

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string,string> Events = new Dictionary<string, string>();
+    
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
             return View();
         }
 
@@ -25,9 +27,49 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string desc)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name,desc);
+            EventData.Add(newEvent);
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            //EventData.Delete()
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Delete")]
+        public IActionResult DeleteEvents(int[] eventIds)
+        {
+            foreach(int id in eventIds)
+            {
+                EventData.Delete(id);
+            }
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int Id)
+        {
+            // controller code will go here
+            ViewBag.eventToEdit = EventData.GetById(Id);
+            return View();
+
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult Edit(int id, string name, string description)
+        {
+            //Console.WriteLine("Inside post submit method");
+            // controller code will go here
+            EventData.Update(id, name, description);
+
             return Redirect("/Events");
         }
     }
---
+++ Data/EventData.cs
@@ -0,0 +1,44 @@
+﻿using CodingEvents.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Data
+{
+    public class EventData
+    {
+        //store the events
+        private static Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+        //add an event
+        public static void Add(Event newEvent)
+        {
+            Events.Add(newEvent.Id, newEvent);
+        }
+
+        //delete an event
+        public static void Delete(int id)
+        {
+            Events.Remove(id);
+        }
+        
+        //retrieve events
+        public static IEnumerable<Event> GetAll()
+        {
+            return Events.Values;
+        }
+
+        //retrieve an event
+        public static Event GetById(int id)
+        {
+            return Events.GetValueOrDefault(id);
+        }
+
+        public static void Update(int id, string name, string desc)
+        {
+            EventData.GetById(id).Name = name;
+            EventData.GetById(id).Description = desc;
+        }
+    }
+}
